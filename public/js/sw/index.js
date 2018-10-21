11ba3cbeba6b53e48f866a4efc5ff935c0cf224d
Funmito <funmito.odefemi@outlook.com>
2018-10-21 20:26:06 +0100

    add example of hijacking service worker requests

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,11 @@
 self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+    // TODO: respond to all requests with an html response
+    // containing an element with class ="a-winner-is-me".
+    // Ensure the Content-Type of the response is "text/html"
+    event.respondWith(
+        new Response('<div class="a-winner-is-me">This is my first experience hijacking a serviceWorker request</div>', {
+            headers: { 'Content-Type': 'text/html' } // pass in the Content-Type as a header
+        })
+    );
+    console.log(event.request);
 });
