d7e23908a8de2e621efc63866c3b30a7ea372f7f
Funmito <funmito.odefemi@outlook.com>
2018-10-21 22:44:23 +0100

    add solution to gif-respomse quiz

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,11 @@
 self.addEventListener('fetch', function(event) {
-  // TODO: only respond to requests with a
-  // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+    // TODO: only respond to requests with a
+    // url ending in ".jpg"
+    if (event.request.url.endsWith('.jpg')) {
+        event.respondWith(
+            fetch('/imgs/dr-evil.gif')
+        );
+    }
+    console.log(event.request);
+
 });
