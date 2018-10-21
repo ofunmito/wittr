7718cb7bbaf20ab02b1c247b41bd60e5d382f68a
Funmito <funmito.odefemi@outlook.com>
2018-10-21 23:29:40 +0100

    add solution to error habdling quiz

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,15 +1,15 @@
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).then(function(response) {
-      if (response.status === 404) {
-        // TODO: instead, respond with the gif at
-        // /imgs/dr-evil.gif
-        // using a network request
-        return new Response("Whoops, not found");
-      }
-      return response;
-    }).catch(function() {
-      return new Response("Uh oh, that totally failed!");
-    })
-  );
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            if (response.status === 404) {
+                // TODO: instead, respond with the gif at
+                // Offline/imgs/dr-evil.gif
+                // using a network request
+                return fetch('/imgs/dr-evil.gif');
+            }
+            // return response;
+        }).catch(function() {
+            return new Response("Uh oh, that totally failed!");
+        })
+    );
 });
