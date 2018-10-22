9d2927e4400780619ed542184bf4c4e7e0fae434
Funmito <funmito.odefemi@outlook.com>
2018-10-22 14:09:04 +0100

    add caching for page skeleton

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,50 +1,58 @@
-var staticCacheName = 'wittr-static-v3';
+var staticCacheName = 'wittr-static-v4';
 
 self.addEventListener('install', function(event) {
-  // TODO: cache /skeleton rather than the root page
+    // TODO: cache /skeleton rather than the root page
 
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'css/main.css',
-        'imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                '/skeleton',
+                'js/main.js',
+                'css/main.css',
+                'imgs/icon.png',
+                'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+                'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('wittr-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('wittr-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-  // TODO: respond to requests for the root page with
-  // the page skeleton from the cache
+    // TODO: respond to requests for the root page with
+    // the page skeleton from the cache
+    let requestURL = new URL(event.request.url);
 
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+    if (requestURL.origin === location.origin) {
+        if (requestURL.pathname === '/') {
+            event.respondWith(caches.match('/skeleton'));
+            return;
+        }
+    }
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+
 });
 
 self.addEventListener('message', function(event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
 });
