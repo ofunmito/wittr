c7182a512733f1935cd5b9ef1ebfcc67044e6b63
Funmito <funmito.odefemi@outlook.com>
2018-10-22 12:31:17 +0100

    add UX update notification

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -1,93 +1,117 @@
 import PostsView from './views/Posts';
 import ToastsView from './views/Toasts';
 import idb from 'idb';
+import { worker } from 'cluster';
 
 export default function IndexController(container) {
-  this._container = container;
-  this._postsView = new PostsView(this._container);
-  this._toastsView = new ToastsView(this._container);
-  this._lostConnectionToast = null;
-  this._openSocket();
-  this._registerServiceWorker();
+    this._container = container;
+    this._postsView = new PostsView(this._container);
+    this._toastsView = new ToastsView(this._container);
+    this._lostConnectionToast = null;
+    this._openSocket();
+    this._registerServiceWorker();
 }
 
 IndexController.prototype._registerServiceWorker = function() {
-  if (!navigator.serviceWorker) return;
+    if (!navigator.serviceWorker) return;
 
-  var indexController = this;
+    var indexController = this;
 
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
-    // TODO: if there's no controller, this page wasn't loaded
-    // via a service worker, so they're looking at the latest version.
-    // In that case, exit early
+    navigator.serviceWorker.register('/sw.js').then(function(reg) {
+        // TODO: if there's no controller, this page wasn't loaded
+        // via a service worker, so they're looking at the latest version.
+        // In that case, exit early
+        if (!navigator.serviceWorker.controller) {
+            return;
+        }
+        // TODO: if there's an updated worker already waiting, call
+        // indexController._updateReady()
+        if (reg.waiting) {
+            indexController._updateReady();
+            return;
+        }
 
-    // TODO: if there's an updated worker already waiting, call
-    // indexController._updateReady()
+        // TODO: if there's an updated worker installing, track its
+        // progress. If it becomes "installed", call
+        // indexController._updateReady()
+        if (reg.installing) {
+            indexController._trackInstalling(reg.installing);
+            return;
+        }
 
-    // TODO: if there's an updated worker installing, track its
-    // progress. If it becomes "installed", call
-    // indexController._updateReady()
-
-    // TODO: otherwise, listen for new installing workers arriving.
-    // If one arrives, track its progress.
-    // If it becomes "installed", call
-    // indexController._updateReady()
-  });
+        // TODO: otherwise, listen for new installing workers arriving.
+        // If one arrives, track its progress.
+        // If it becomes "installed", call
+        // indexController._updateReady()
+        reg.addEventListener('updatefound', function() {
+            indexController._trackInstalling(reg.installing);
+        });
+    });
 };
 
+IndexController.prototype._trackInstalling = function() {
+    var indexController = this;
+
+    worker.addEventListener('statechange', function() {
+        if (worker.state == 'installed') {
+            indexController._updateReady();
+        }
+    })
+}
+
 IndexController.prototype._updateReady = function() {
-  var toast = this._toastsView.show("New version available", {
-    buttons: ['whatever']
-  });
+    var toast = this._toastsView.show("New version available", {
+        buttons: ['whatever']
+    });
 };
 
 // open a connection to the server for live updates
 IndexController.prototype._openSocket = function() {
-  var indexController = this;
-  var latestPostDate = this._postsView.getLatestPostDate();
+    var indexController = this;
+    var latestPostDate = this._postsView.getLatestPostDate();
 
-  // create a url pointing to /updates with the ws protocol
-  var socketUrl = new URL('/updates', window.location);
-  socketUrl.protocol = 'ws';
+    // create a url pointing to /updates with the ws protocol
+    var socketUrl = new URL('/updates', window.location);
+    socketUrl.protocol = 'ws';
 
-  if (latestPostDate) {
-    socketUrl.search = 'since=' + latestPostDate.valueOf();
-  }
-
-  // this is a little hack for the settings page's tests,
-  // it isn't needed for Wittr
-  socketUrl.search += '&' + location.search.slice(1);
-
-  var ws = new WebSocket(socketUrl.href);
-
-  // add listeners
-  ws.addEventListener('open', function() {
-    if (indexController._lostConnectionToast) {
-      indexController._lostConnectionToast.hide();
+    if (latestPostDate) {
+        socketUrl.search = 'since=' + latestPostDate.valueOf();
     }
-  });
 
-  ws.addEventListener('message', function(event) {
-    requestAnimationFrame(function() {
-      indexController._onSocketMessage(event.data);
+    // this is a little hack for the settings page's tests,
+    // it isn't needed for Wittr
+    socketUrl.search += '&' + location.search.slice(1);
+
+    var ws = new WebSocket(socketUrl.href);
+
+    // add listeners
+    ws.addEventListener('open', function() {
+        if (indexController._lostConnectionToast) {
+            indexController._lostConnectionToast.hide();
+        }
     });
-  });
 
-  ws.addEventListener('close', function() {
-    // tell the user
-    if (!indexController._lostConnectionToast) {
-      indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
-    }
+    ws.addEventListener('message', function(event) {
+        requestAnimationFrame(function() {
+            indexController._onSocketMessage(event.data);
+        });
+    });
 
-    // try and reconnect in 5 seconds
-    setTimeout(function() {
-      indexController._openSocket();
-    }, 5000);
-  });
+    ws.addEventListener('close', function() {
+        // tell the user
+        if (!indexController._lostConnectionToast) {
+            indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
+        }
+
+        // try and reconnect in 5 seconds
+        setTimeout(function() {
+            indexController._openSocket();
+        }, 5000);
+    });
 };
 
 // called when the web socket sends message data
 IndexController.prototype._onSocketMessage = function(data) {
-  var messages = JSON.parse(data);
-  this._postsView.addPosts(messages);
+    var messages = JSON.parse(data);
+    this._postsView.addPosts(messages);
 };
